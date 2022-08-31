0eb26625d8726a0c657926cb682bbb8df3b7f4d1
soumareIbrahimsada <soumareibrahimsada@gmail.com>
2022-09-01 01:48:58 +0200

    commit jour 3

---
+++ week2/jour3/dailychallege/script1.js
@@ -0,0 +1,6 @@
+//creons un tableau dont les element seront des ligne de 1à 6 étoiles
+let tab=["*","**","***","****","*****","******"];
+//Etablissons une boucle for pour afficher les élements du tableau
+for (let i= 0; i< tab.length ;i++) {
+    console.log(tab[i])
+}
