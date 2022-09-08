4d39ef7a03f27321da95f2157c727bbeb2ddb167
soumareIbrahimsada <soumareibrahimsada@gmail.com>
2022-09-08 18:03:40 +0200

    commit jour 5

--- week2/jour5/dailyChallenge/dailyChallenge.js
+++ week2/jour5/dailyChallenge/dailyChallenge.js
@@ -3,13 +3,25 @@ while (isNaN(nombre)||(nombre<0)||(nombre==="")) {
     var nombre=prompt("veillez saisir le nombre de bouteille sur le mur")
 }
 var j=1
-console.log(nombre+"bottles of beer on the wall")
-    console.log(nombre+"bottles of beer")
-    console.log("Take "+j+" down, pass it around")
-
-while((nombre!==0)||(nombre!==1)||(nombre>=0)) {
-    nombre=Number(nombre)-j
-    j=j+1
+        console.log(nombre+"bottles of beer on the wall")
+        console.log(nombre+"bottles of beer")
+        console.log("Take "+j+" down, pass them around")
+while(nombre>=0){
     console.log(nombre)
+    if (nombre===0) {
+        console.log("0 bottle of beer on the wall")
+        nombre=Number(nombre)-1
+    }else if(nombre===1){
+        console.log(nombre+"bottles of beer on the wall")
+        console.log(nombre+"bottles of beer")
+        console.log("Take 1 down, pass it around")
+        nombre=Number(nombre)-2
+    }else {
+        nombre=Number(nombre)-j
+        console.log(nombre+"bottles of beer on the wall")
+        console.log(nombre+"bottles of beer")
+        j=j+1
+        console.log("Take "+j+" down, pass them around")
+    } 
 }
 
